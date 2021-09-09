495c820f13505c8c28cb0af65f52fd102953dfe0
Ahmad-A92-3 <ahmad.alsyad92@gmail.com>
2021-09-09 21:11:09 +0300

    first commit-create spring initilizer

---
+++ src/main/java/com/example/talkversity/TalkversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.talkversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/talkversity/TalkversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talkversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
